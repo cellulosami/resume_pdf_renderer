4a1b150337e489d18cf16a5a1de4a4606b4fe1b8
austin shields <ajshields196@gmail.com>
2021-04-20 20:35:50 -0400

    install html2pdf.js and create home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,33 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Resume Builder</h1>
+    <div>
+      <p>
+        Student ID: <input v-model="studentID">
+      </p>
+      <p>
+        Resume Format:
+        <select v-model="resumeFormat">
+          <option value="one">One-column</option>
+          <option value="two">Two-column</option>
+        </select>
+      </p>
+      <router-link v-bind:to="`/resumes/${studentID}?columns=${resumeFormat}`">
+        <button>Create PDF</button>
+      </router-link>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data: function () {
+    return {
+      studentID: "",
+      resumeFormat: ""
+    }
   }
 }
 </script>
--- src/views/Show.vue
+++ src/views/Show.vue
@@ -1,108 +1,91 @@
 <template>
-  <p>
-    <button v-on:click="pdfTest">test</button>
-    <br />
-    <br />
-    <br />
-    <br />
-    {{ this.document }}
-  </p>
+  <div>
+    <p>
+      <button v-on:click="pdfTest">test</button>
+    </p>
+    <div id="test">
+      <p>Egg Salad Sandwich</p>
+      <p>Egg Salad Sandwich</p>
+      <p>Egg Salad Sandwich</p>
+      <p>Egg Salad Sandwich</p>
+      <h1>Egg Salad Sandwich</h1>
+      <h1>Egg Salad Sandwich</h1>
+      <h1>Egg Salad Sandwich</h1>
+      <h1>Egg Salad Sandwich</h1>
+      <h1>Egg Salad Sandwich</h1>
+      <h1>Egg Salad Sandwich</h1>
+      <h1>Egg Salad Sandwich</h1>
+      <h1>Egg Salad Sandwich</h1>
+      <h1>Egg Salad Sandwich</h1>
+      <h1>Egg Salad Sandwich</h1>
+      <h1>Egg Salad Sandwich</h1>
+      <h1>Egg Salad Sandwich</h1>
+      <h1>Egg Salad Sandwich</h1>
+      <h1>Egg Salad Sandwich</h1>
+      <h1>Egg Salad Sandwich</h1>
+      <h1>Egg Salad Sandwich</h1>
+      <h1>Egg Salad Sandwich</h1>
+      <h1>Egg Salad Sandwich</h1>
+      <h1>Egg Salad Sandwich</h1>
+      <h1>Egg Salad Sandwich</h1>
+      <h1>Egg Salad Sandwich</h1>
+      <h1>Egg Salad Sandwich</h1>
+      <h1>Egg Salad Sandwich</h1>
+      <h1>Egg Salad Sandwich</h1>
+      <h1>Egg Salad Sandwich</h1>
+      <h1>Egg Salad Sandwich</h1>
+      <h1>Egg Salad Sandwich</h1>
+      <h1>Egg Salad Sandwich</h1>
+      <h1>Egg Salad Sandwich</h1>
+      <h1>Egg Salad Sandwich</h1>
+      <h1>Egg Salad Sandwich</h1>
+      <h1>Egg Salad Sandwich</h1>
+      <h1>Egg Salad Sandwich</h1>
+      <h1>Egg Salad Sandwich</h1>
+      <h1>Egg Salad Sandwich</h1>
+      <h1>Egg Salad Sandwich</h1>
+      <h1>Egg Salad Sandwich</h1>
+      <h1>Egg Salad Sandwich</h1>
+      <h1>Egg Salad Sandwich</h1>
+      <h1>Egg Salad Sandwich</h1>
+      <h1>Egg Salad Sandwich</h1>
+      <h1>Egg Salad Sandwich</h1>
+      <h1>Egg Salad Sandwich</h1>
+      <h1>Egg Salad Sandwich</h1>
+    </div>
+  </div>
 </template>
 
 <script>
+import html2pdf from "html2pdf.js"
 import axios from "axios";
-import { jsPDF } from "jspdf";
 
 export default {
   data: function () {
     return {
-      message: "welcome to show page",
-      dummythiccdata: {},
-      document: "",
-      currentStudent: {
-        first_name: "ted",
-        last_name: "dundy",
-        email: "teddundy@example.com",
-        phone_number: "555-555-5555",
-        short_bio: "I am a human person with human appendages beep boop",
-        linkedin_url: "https://linkedin.com/human",
-        twitter_handle: "teddundyful",
-        personal_website_url: "google.com",
-        online_resume_url: "gooooogle.com",
-        github_url: "gooooooooogle.com",
-        photo: "image.url.com",
-        experiences: [
-          {
-            start_date: "Jan 2021",
-            end_date: "May 2021",
-            job_title: "Software developer",
-            company_name: "Google",
-            details: "a lot of text"
-          },
-          {
-            start_date: "Jan 2027",
-            end_date: "May 2027",
-            job_title: "Software developer GUY MAN PERSON",
-            company_name: "Google Glass",
-            details: "a wee bit o' text laddy"
-          }
-        ],
-        capstones: [{
-            description: "great capstone",
-            url: "www.url.com",
-            screenshot: "www.screenshot.com",
-          },
-          {
-            description: "okay capstone",
-            url: "www.url2.com",
-            screenshot: "www.screenshot2.com",
-          },
-        ],
-        educations: [{
-            start_date: "2",
-            end_date: "3",
-            degree: "BS in BS",
-            university_name: "Havard University",
-            details: "The greatest and finest college in the US"
-          },
-          {
-            start_date: "2222",
-            end_date: "33333",
-            degree: "BS in BS in BS in BS in BS",
-            university_name: "HavRODUniversityssss",
-            details: "The greatest and finest college in the US other than that one"
-          }
-        ],
-        skills: [{
-            skill_name_1: "very good",
-            skill_name_2: "excellent",
-            skill_name_3: "good stuff",
-            skill_name_4: "good coder"
-          }
-        ]
-      }
+      currentStudent: {},
     }
   },
   mounted: function () {
     console.log("mounted");
     axios
-      .get("/api/products/1")
+      .get("/api/students/" + this.$route.params.id)
       .then(response => {
-        console.log("hooray!");
-        this.dummythiccdata = response.data;
+        console.log("hooray");
       })
       .catch(error => {
-        console.log("uh oh");
-      })
+        console.log("boo");
+      });
   },
   methods: {
     pdfTest: function () {
       console.log("pdftest");
-      this.document = new jsPDF();
-
-      this.document.text(this.currentStudent.first_name, 10, 150);
-      console.log(this.document);
-      this.document.save("a4.pdf");
+      var element = document.getElementById('test');
+      var opt = {
+        margin: 8,
+        // pagebreak: { mode: 'avoid-all' }
+      }
+      html2pdf().set(opt).from(element).save();
     }
   }
 }
